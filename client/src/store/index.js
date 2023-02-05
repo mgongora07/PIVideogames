3314b15cfe396592dbe892645d10701f250dfe9a
mgongora07 <>
2023-02-05 12:46:30 -0600

    dp

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,12 @@
-import {createStore,applyMiddleware} from "redux";
-import{composeWithDevTools} from "redux-devtools-extension";
+import {createStore,applyMiddleware,compose} from "redux";
+//import{composeWithDevTools} from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "../reducer";
 
-const store=createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(thunk))
-    );
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const store=createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)));
+
+
 
     export default store;
 
